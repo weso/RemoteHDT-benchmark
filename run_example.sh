281fe656415c303d0de4df2c8bd1511d25602181
Diego Martín Fernández <diegomartinfnz@gmail.com>
2024-06-17 11:50:09 +0200

    change

--- run_example.sh
+++ run_example.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-# ./reset.sh 
-# ./generate_RDF.sh n50-p10-t300 50 10 300
-# sudo ./start_remote_server_simulation.sh
-# ./execute_benchmark.sh all 5
-# sudo ./stop_remote_server_simulation.sh
-
-
 ./reset.sh 
 ./generate_RDF.sh moreNodes-n1000-p10-t1000000 1000 10 1000000
 ./generate_RDF.sh moreNodes-n10000-p10-t1000000 10000 10 1000000
